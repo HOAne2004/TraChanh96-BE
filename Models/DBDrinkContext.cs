c518a0087ab985a1d4ca4296c38611f3f40a32f4
HOAn04 <lehuyhoan04@gmail.com>
2025-11-27 00:46:15 +0700

    Them global.json de fix loi build Railway

--- Models/DBDrinkContext.cs
+++ Models/DBDrinkContext.cs
@@ -66,10 +66,6 @@ public partial class DBDrinkContext : DbContext
 
     public virtual DbSet<VoucherTemplate> VoucherTemplates { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-2BE7M5SS\\SQLEXPRESS;Database=DBDrink;User Id=hoan;Password=HOAn2004;Encrypt=False;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Brand>(entity =>
